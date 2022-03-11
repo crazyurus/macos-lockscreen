5af05da3fc5787d7e274b1f8559c001cc19a2270
Crazy Urus <crazyurus@vip.qq.com>
2022-03-11 15:35:03 +0800

    feat: 支持版本号参数

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+#define VERSION "1.0.4"
+
 extern int SACLockScreenImmediate(void);
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc == 2) {
+    if (
+      strcmp(argv[1], "-v") == 0 ||
+      strcmp(argv[1], "--version") == 0
+    ) {
+      printf("%s\n", VERSION);
+      return 0;
+    }
+  }
+
   return SACLockScreenImmediate();
 }
